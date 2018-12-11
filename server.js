364a7e3cbf5693c51bf83e76b5fb560606166a9f
Juan Nenna <juan.a.nenna@gmail.com>
2018-12-11 16:12:39 +0100

    now its running at least locally

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
 const app = require('./lib/app');
 
 module.exports.run = app;
+
+const server = app.listen(process.env.PORT || 8080, function () {
+  const port = server.address().port;
+  console.log("App now running on port", port);
+});
